031c57f2ec26994052957e57f3c7e282427760f4
Massam Khan <hafizmassamtabraizkhan@gmail.com>
2023-06-16 20:22:01 +0500

    API implementation

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,22 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { remveBook } from '../redux/books/booksSlice';
+import { fetchBook, deleteBook, remveBook } from '../redux/books/booksSlice';
 import Book from './Book';
 
 const BookList = () => {
-  const books = useSelector((state) => state.books);
+  const { books, isLoading, error } = useSelector((store) => store.books);
   const dispatch = useDispatch();
 
   const handleDelete = (id) => {
     dispatch(remveBook({ id }));
+    dispatch(deleteBook({ id }));
   };
+
+  useEffect(() => { dispatch(fetchBook()); }, [dispatch]);
+  if (isLoading) {
+    return (
+      <>
+        <h1>is loading</h1>
+      </>
+    );
+  }
+  if (error) {
+    <h1>something went work</h1>;
+  }
 
   return (
     <ul>
       {books.map((book) => (
         <Book
-          key={book.id}
-          id={book.id}
+          key={book.item_id}
+          id={book.item_id}
           title={book.title}
           author={book.author}
           category={book.category}
